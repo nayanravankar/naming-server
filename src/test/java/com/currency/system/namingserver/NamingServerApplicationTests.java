a242a51fd01f9196eac9f71c0edda4f311196652
Nayan Ravankar <nayan.ravankar@amdocs.com>
2021-09-12 16:06:36 +0530

    naming server base code

---
+++ src/test/java/com/currency/system/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currency.system.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
